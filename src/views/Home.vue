90b4ab404043bec6ca9818a45cc1de1f79d88f71
旋转跳跃 <Hxbnzy@gmail.com>
2019-06-18 23:12:15 +0800

    项目创建

---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Home extends Vue.Component<{}> {
+  render() {
+      return(
+          <div>123123</div>
+      )
+  }
+}
